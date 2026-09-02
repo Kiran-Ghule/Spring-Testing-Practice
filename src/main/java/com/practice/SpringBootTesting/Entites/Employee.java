63cb6657b51c8a37d8205be9989e5e33492a91c3
Kiran-Ghule <ghulekiran1608@gmail.com>
2026-09-03 00:02:19 +0530

    @Data added and Type Change From int to Long

--- src/main/java/com/practice/SpringBootTesting/Entites/Employee.java
+++ src/main/java/com/practice/SpringBootTesting/Entites/Employee.java
@@ -2,13 +2,15 @@ package com.practice.SpringBootTesting.Entites;
 
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
