7edf2fc0a6add6471efa22fe8f22de80b54770f6
Kiran-Ghule <ghulekiran1608@gmail.com>
2026-09-02 23:53:52 +0530

    Finding Employee By Id Method Created

--- src/main/java/com/practice/SpringBootTesting/Services/EmpService.java
+++ src/main/java/com/practice/SpringBootTesting/Services/EmpService.java
@@ -1,4 +1,14 @@
 package com.practice.SpringBootTesting.Services;
 
+import com.practice.SpringBootTesting.DTO.EmployeeDTO;
+import com.practice.SpringBootTesting.Entites.Employee;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface EmpService {
+
+    public EmployeeDTO getEmployee(Long id);
+    public void deleteEmployee(Long id);
+    public EmployeeDTO createEmployee(EmployeeDTO emp);
+    public EmployeeDTO updateEmployee(Long id,EmployeeDTO emp);
 }
--- src/main/java/com/practice/SpringBootTesting/Services/Implementations/EmpServiceImp.java
+++ src/main/java/com/practice/SpringBootTesting/Services/Implementations/EmpServiceImp.java
@@ -1,4 +1,40 @@
 package com.practice.SpringBootTesting.Services.Implementations;
 
-public class EmpServiceImp {
+import com.practice.SpringBootTesting.DTO.EmployeeDTO;
+import com.practice.SpringBootTesting.Entites.Employee;
+import com.practice.SpringBootTesting.Exceptions.EmployeeNotFound;
+import com.practice.SpringBootTesting.Repositories.EmployeeRepo;
+import com.practice.SpringBootTesting.Services.EmpService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@RequiredArgsConstructor
+public class EmpServiceImp implements EmpService {
+    private final EmployeeRepo  empRepo;
+    private final ModelMapper mapper;
+
+    @Override
+    public EmployeeDTO getEmployee(Long id) {
+        Employee emp = empRepo.findById(id)
+                .orElseThrow(
+                        ()-> new EmployeeNotFound("Employee Not Found with id: " + id));
+        EmployeeDTO empDTO = mapper.map(emp, EmployeeDTO.class);
+        return empDTO;
+
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+
+    }
+
+    @Override
+    public EmployeeDTO createEmployee(EmployeeDTO emp) {
+        return null;
+    }
+
+    @Override
+    public EmployeeDTO updateEmployee(Long id, EmployeeDTO emp) {
+        return null;
+    }
 }
