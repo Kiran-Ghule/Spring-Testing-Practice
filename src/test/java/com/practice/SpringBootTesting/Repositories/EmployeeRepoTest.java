558bf77a9afbbc3bb072be249793305d7b118922
Kiran-Ghule <ghulekiran1608@gmail.com>
2026-09-03 21:26:17 +0530

    Test for EmployeeRepo initiated

--- src/test/java/com/practice/SpringBootTesting/Repositories/EmployeeRepoTest.java
+++ src/test/java/com/practice/SpringBootTesting/Repositories/EmployeeRepoTest.java
@@ -1,4 +1,22 @@
+package com.practice.SpringBootTesting.Repositories;
+
+import com.practice.SpringBootTesting.Entites.Employee;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
 class EmployeeRepoTest {
-  
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
+    private Employee employee;
+
+    @Test
+    void findEmployee_IFValidEmail_ByEmail() {
+
+    }
 }
