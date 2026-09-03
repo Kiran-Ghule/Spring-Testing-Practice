c343bfc75ed1e63e86551967a2363111740bcf47
Kiran-Ghule <ghulekiran1608@gmail.com>
2026-09-03 21:35:40 +0530

    Unit Testing is done

--- src/test/java/com/practice/SpringBootTesting/Repositories/EmployeeRepoTest.java
+++ src/test/java/com/practice/SpringBootTesting/Repositories/EmployeeRepoTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -15,8 +18,25 @@ class EmployeeRepoTest {
 
     private Employee employee;
 
+    @BeforeEach
+    public void setup() {
+        employee = new Employee(null, "Samay","samayara@gmail.com",10000L);
+    }
+
     @Test
     void findEmployee_IFValidEmail_ByEmail() {
+            employeeRepo.save(employee);
+           Optional<Employee> employee1= employeeRepo.findByEmail(employee.getEmail());
+
+           assertThat(employee1.get().getEmail()).isEqualTo(employee.getEmail());
+    }
+
+    @Test
+    void findEmployee_IFInvalidEmail_ByEmail() {
+        employeeRepo.save(employee);
+
+        Optional<Employee> employee1= employeeRepo.findByEmail(employee.getEmail()+"edu.in");
+        assertThat(employee1.get().getEmail()).isBlank().isEmpty();
 
     }
 }
