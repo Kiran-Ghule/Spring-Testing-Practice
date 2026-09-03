d2eb57e88fb43e8d1ebdd502297852585f0d0cd4
Kiran-Ghule <ghulekiran1608@gmail.com>
2026-09-03 21:24:15 +0530

    Data type changed

--- src/main/java/com/practice/SpringBootTesting/Entites/Employee.java
+++ src/main/java/com/practice/SpringBootTesting/Entites/Employee.java
@@ -2,10 +2,14 @@ package com.practice.SpringBootTesting.Entites;
 
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
 
     @Id
